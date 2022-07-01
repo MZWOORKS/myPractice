4eb64ead7b141229627f43f211f0642c3dc09a00
MiguelZ <zacaula0537@gmail.com>
2022-07-01 16:17:06 -0500

    7-1-22

--- index.js
+++ index.js
@@ -55,3 +55,119 @@ if (age >= 18) {
 }
 
 // --------------------------------------------------------------
+
+// -- checked property -----
+
+document.getElementById("myButton").onclick = function () {
+  const visaBtn = document.getElementById("visaBtn");
+  const masterCardBtn = document.getElementById("masterCardBtn");
+  const dicoverBtn = document.getElementById("discoverBtn");
+
+  //   const myCheckBox = document.getElementById("myCheckBox");
+  //   if (myCheckBox.checked) {
+  //     alert("youre suscribe");
+  //   } else {
+  //     alert("your not suscribe");
+  //   }
+
+  if (visaBtn.checked) {
+    alert("paying with visa");
+  } else if (masterCardBtn.checked) {
+    alert("paying with mastercard");
+  } else if (discoverBtn.checked) {
+    alert("paying with Dicover");
+  } else {
+    alert("you must select payment type");
+  }
+};
+
+// -------------------------------------------------------------
+
+// ************** SWITCH
+
+let grade = "a";
+grade = grade.toUpperCase();
+
+switch (grade) {
+  case "A":
+    console.log("you did great");
+    break;
+  case "B":
+    console.log("you did good");
+    break;
+  case "C":
+    console.log("you did ok ");
+    break;
+  default:
+    console.log(grade, "is not a letter grade");
+}
+
+// --------------------------------------------------------------
+
+// ********* && AND || OR
+
+// let temp = 25;
+// let sunny = true;
+
+// if (temp > 0 && temp < 30 && sunny) {
+//   alert("the weather is good");
+// } else {
+//   alert("the wether is bad");
+// }
+
+// ----------------------------------------------------
+
+// *********** ! NOT Logical Operator
+
+// let temp = 19;
+
+// if (!(temp > 0)) {
+//   alert("is cold outside");
+// } else {
+//   alert("is warm outside");
+// }
+
+// ------------------------------------------------------------
+
+// *********** WHILE LOOP
+
+// let userName = "";
+
+// while (userName == "" || userName == null) {
+//   userName = window.prompt("enter your name");
+// }
+
+// console.log("Hello", userName);
+
+// ---------------------------------------------------------------------------
+
+// *********** DO WHILE LOOP
+
+// let userName;
+
+// do {
+//   userName = window.prompt("enter your name");
+// } while (userName == "" || userName == null);
+
+// console.log("Hello", userName);
+
+// ------------------------------------------------------------------------
+
+// *********** FOR LOOP
+
+//  for (let i = 1; i <= 10; i += 1) {
+//   console.log(i);
+// }
+
+// ------------------------------------------------------------------------
+
+// *********** Break, Continue
+
+for (let i = 1; i <= 20; i += 1) {
+  if (i == 13) {
+    continue;
+  }
+  console.log(i);
+}
+
+// ------------------------------------------------------------------------
