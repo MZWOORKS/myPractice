f201eb2e72fc74b060502e527f30b4ac4ae18159
MiguelZ <zacaula0537@gmail.com>
2022-07-11 20:39:50 -0500

    7-11-22

--- index.js
+++ index.js
@@ -340,6 +340,7 @@
 // ------------------------------------------------------------------------
 
 // *********** ARRAYS
+
 // array = think of it as a variable
 //         that can store multiple values
 
@@ -367,5 +368,259 @@
 // for (let fruit of fruits) {
 //   console.log(fruit);
 // }
+
+// ------------------------------------------------------------------------
+
+// *********** 2D ARRAYS
+
+// let fruits = ["aple", "pranges", "bananas"];
+// let vegetables = ["carrots", "onion", "potatoes"];
+// let meats = ["eggs", "chicken", "fish"];
+
+// let groceryList = [fruits, vegetables, meats];
+
+// groceryList[2][2] = "mangos";
+
+// for (let list of groceryList) {
+//   for (let food of list) {
+//     console.log(food);
+//   }
+// }
+
+// ------------------------------------------------------------------------
+
+// *********** SPREAD OPERATOR
+// spread operator = allows an iterable such as an
+//                   array or string to be expanded
+//                   in places where zero or more
+//                   arguments are expected
+//                   (unpacks the elements)
+
+// let userName = "Miguel Zacaula";
+// let nunbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(...userName);
+
+// ------------------------------------------------------------------------
+
+// *********** REST PARAMETERS
+// rest parameters = represents an indefinite number
+//                   of parameters
+//                   (pack arguments into an array)
+//
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+// let e = 5;
+
+// console.log(sum(a, b, c));
+
+// function sum(...numbers) {
+//   let total = 0;
+//   for (let number of numbers) {
+//     total += number;
+//   }
+//   return total;
+// }
+
+// ------------------------------------------------------------------------
+
+// ***********   CALLBACK
+
+// callback = a function passed as an argument
+//            to another function
+//
+//     Ensures that a function is not going
+//     to run before a task is completed.
+//     Helps us develop asynchronus code.
+//     (When one funtion has to wait for another function)
+//     that helps us avoid errors and potential problems
+//     Ex. Wait for a file to load
+
+// let total = sum(2, 3);
+// displayConsole(total);
+// displayDOM(total);
+
+// function sum(x, y) {
+//   let result = x + y;
+//   return result;
+// }
+
+// function displayConsole(output) {
+//   console.log(output);
+// }
+
+// function displayDOM(output) {
+//   document.getElementById("myLabelCallBack").innerHTML = output;
+// }
+
+// ##########################CALLBACK EXEMPLE
+
+// sum(2, 3, displayConsole);
+
+// function sum(x, y, callback) {
+//   let result = x + y;
+//   callback(result);
+// }
+
+// function displayConsole(output) {
+//   console.log(output);
+// }
+
+// function displayDOM(output) {
+//   document.getElementById("myLabelCallBack").innerHTML = output;
+// }
+
+// ------------------------------------------------------------------------
+
+// *********** ARRAY.FOREACH() = executes a provided callback function
+//                               once for each array element
+
+// let students = ["spongebob", "patrick", "squidward"];
+// students.forEach(capitalize);
+// students.forEach(print);
+
+// function capitalize(element, index, array) {
+//   array[index] = element[0].toUpperCase() + element.substring(1);
+// }
+
+// function print(element) {
+//   console.log(element);
+// }
+// ------------------------------------------------------------------------
+
+// *********** ARRAY.MAP()
+// array.map() = executes a privided callback funtion
+//               once for each array element
+//               AND creates a new array
+
+// let numbers = [1, 2, 3, 4, 5];
+// let squares = numbers.map(square);
+
+// squares.forEach(print);
+
+// function square(element) {
+//   return Math.pow(element, 2);
+// }
+
+// function print(element) {
+//   console.log(element);
+// }
+
+// ------------------------------------------------------------------------
+
+// *********** ARRAY.FILTER
+// array.filter() = creates a new array with all elements
+//                  that pass the test provided by a function
+//
+
+// let ages = [18, 14, 43, 44, 13];
+// let adults = ages.filter(checkAge);
+
+// adults.forEach(print);
+
+// function checkAge(element) {
+//   return element >= 18;
+// }
+
+// function print(element) {
+//   console.log(element);
+// }
+
+// ------------------------------------------------------------------------
+
+// ***********ARRAY.REDUCE()
+// array.reduce() = reduces an array to a single value
+
+// let prices = [4, 5, 12, 44, 55];
+// let total = prices.reduce(checkOut);
+
+// console.log(`the total is ${total}`);
+
+// function checkOut(total, element) {
+//   return total + element;
+// }
+
+// ------------------------------------------------------------------------
+
+// *********** SORT NUMBERS
+
+// let grades = [19, 33, 22, 68, 54, 99];
+
+// grades = grades.sort(descendingSort);
+// grades.forEach(print);
+
+// function descendingSort(x, y) {
+//   return y - x;
+// }
+
+// function print(element) {
+//   console.log(element);
+// }
+
+// ------------------------------------------------------------------------
+
+// *********** FUNCTION EXPRESSION
+// function expression = function without a name(anonymous function)
+//                       avoid polluting the global scope with names
+//                       Write it, then forget about it.
+
+// const greeting = function () {
+//   console.log("hello");
+// };
+
+// greeting();
+
+// ------------------------------------------------------------------------
+
+// *********** ARROW FUNCTIONS
+// arrow function = compact alternative to a traditional function
+//                =>
+
+// REGULAR FUNCTION???????-------
+// const greating = function (userName) {
+//   console.log(`hello ${userName}`);
+// };
+
+// greating("mike");
+// ++++++++++++>>>>>>>>>>>>>>>>>>>>>>>
+
+// Arrow function aqui abajito
+// const greating = (userName) => console.log(`Hello ${userName}`);
+
+// greating("mike");
+
+// let grades = [100, 50, 90, 60, 80, 90];
+
+// grades.sort((x, y) => y - x);
+// grades.forEach((element) => console.log(element));
+
+// ==================>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// ------------------------------------------------------------------------
+
+// *********** SHUFFLE AN ARRAY
+
+let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+shuffle(cards);
+
+cards.forEach((card) => console.log(card));
+
+function shuffle(array) {
+  let currentIndex = array.lenght;
+
+  while (currentIndex != 0) {
+    let randomIndex = Math.floor(Math.random() * array.lenght);
+    currentIndex -= 1;
+
+    let temp = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temp;
+  }
+
+  return array;
+}
 
 // ------------------------------------------------------------------------
